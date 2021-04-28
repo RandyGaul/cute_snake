80c22f1c4326d51c864c6a50aa613f85f2cbcea1
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2021-04-27 20:28:25 -0700

    most gameplay done

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ using namespace cute;
 #define CUTE_PATH_IMPLEMENTATION
 #include <cute/cute_path.h>
 
+#include <time.h> // time
+
 app_t* app;
 batch_t* b;
 
@@ -197,7 +199,7 @@ void title_screen(app_t* app)
 		title.draw(b);
 		batch_flush(b);
 
-		t += dt;
+		t += dt * 1.25f;
 		float slice_size = (CUTE_PI / 16.0f) * 0.75f;
 		float r = 9;
 		v2 c = v2(26, 13);
@@ -231,6 +233,265 @@ void title_screen(app_t* app)
 	}
 }
 
+sprite_t wall;
+sprite_t weak_wall;
+sprite_t bomb;
+sprite_t snake_head;
+sprite_t snake_segment;
+sprite_t apple;
+v2 dir = v2(1, 0);
+int snake_x;
+int snake_y;
+array<int> segments_x;
+array<int> segments_y;
+bool has_apple = false;
+bool has_bomb = false;
+int apple_x;
+int apple_y;
+int bomb_x;
+int bomb_y;
+
+int current_level = 0;
+array<array<array<int>>> levels = {
+	{
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+	},
+	{
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+		{ 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, },
+	},
+};
+
+int s_snake_spawn_x()
+{
+	array<array<int>>& level = levels[current_level];
+	for (int y = 0; y < level.size(); ++y) {
+		for (int x = 0; x < level[y].size(); ++x) {
+			if (level[y][x] == 1) {
+				return x;
+			}
+		}
+	}
+	CUTE_ASSERT(false);
+	return 0;
+}
+
+int s_snake_spawn_y()
+{
+	array<array<int>>& level = levels[current_level];
+	for (int y = 0; y < level.size(); ++y) {
+		for (int x = 0; x < level[y].size(); ++x) {
+			if (level[y][x] == 1) {
+				return y;
+			}
+		}
+	}
+	CUTE_ASSERT(false);
+	return 0;
+}
+
+void do_gameplay(coroutine_t* co)
+{
+	rnd_t rnd = rnd_seed((uint64_t)time(0));
+	audio_t* eat = audio_load_wav("eat.wav");
+
+	snake_x = s_snake_spawn_x();
+	snake_y = s_snake_spawn_y();
+
+	bool done = false;
+	while (!done) {
+		coroutine_wait(co, 0.5f);
+
+		// Spawn a random apple if one does not already exist.
+		if (!has_apple) {
+			has_apple = true;
+			array<array<int>>& level = levels[current_level];
+			while (1) {
+				apple_x = rnd_next_range(rnd, 0, 15);
+				apple_y = rnd_next_range(rnd, 0, 11);
+
+				// Try again if spawned on walls or the player.
+				int tile = level[apple_y][apple_x];
+				bool on_snake = apple_x == snake_x && apple_y != snake_y;
+				bool on_bomb = has_bomb && snake_x == bomb_x && snake_y == bomb_y;
+				if (tile != 2 && tile != 3 && !on_snake && !on_bomb) {
+					break;
+				}
+			}
+		}
+
+		// Move the snake head.
+		int prev_snake_x = snake_x;
+		int prev_snake_y = snake_y;
+		snake_x += (int)dir.x;
+		snake_y += (int)dir.y;
+
+		// Screen wrapping.
+		if (snake_x < 0) {
+			snake_x = 15;
+		} else if (snake_x > 15) {
+			snake_x = 0;
+		}
+		if (snake_y < 0) {
+			snake_y = 11;
+		} else if (snake_y > 11) {
+			snake_y = 0;
+		}
+
+		// Die conditions.
+		bool hit_self = false;
+		for (int i = 0; i < segments_x.size(); ++i) {
+			if (snake_x == segments_x[i] && snake_y == segments_y[i]) {
+				hit_self = true;
+				break;
+			}
+		}
+		array<array<int>>& level = levels[current_level];
+		bool hit_wall = level[snake_y][snake_x] == 2;
+		bool ran_out_of_segments = false;
+		if (level[snake_y][snake_x] == 3) {
+			// Breakable walls.
+			level[snake_y][snake_x] = 4;
+			if (segments_x.count()) {
+				segments_x.pop();
+				segments_y.pop();
+			} else {
+				ran_out_of_segments = true;
+			}
+		}
+		if (hit_self || hit_wall || ran_out_of_segments) {
+			segments_x.clear();
+			segments_y.clear();
+			has_apple = false;
+			has_bomb = false;
+			snake_x = s_snake_spawn_x();
+			snake_y = s_snake_spawn_y();
+
+			// Weaker walls go from 3 to 4 when hit. Reset them here.
+			for (int y = 0; y < level.size(); ++y) {
+				for (int x = 0; x < level[y].size(); ++x) {
+					if (level[y][x] == 4) {
+						level[y][x] = 3;
+					}
+				}
+			}
+		}
+
+		// Spawn bomb.
+		if (segments_x.size() >= 5 && has_bomb == false) {
+			has_bomb = true;
+			while (1) {
+				bomb_x = rnd_next_range(rnd, 0, 15);
+				bomb_y = rnd_next_range(rnd, 0, 11);
+
+				// Try again if spawned on walls or the player.
+				int tile = level[bomb_y][bomb_x];
+				bool on_snake = bomb_x == snake_x && bomb_y != snake_y;
+				bool on_apple = has_apple && apple_x == bomb_x && apple_y != bomb_y;
+				if (tile != 2 && tile != 3 && !on_snake && !on_apple) {
+					break;
+				}
+			}
+		}
+
+		// Eat apple.
+		if (snake_x == apple_x && snake_y == apple_y) {
+			sound_play(app, eat);
+			has_apple = false;
+
+			// Grow snake.
+			if (segments_x.size()) {
+				segments_x.add(segments_x.last());
+				segments_y.add(segments_y.last());
+			} else {
+				segments_x.add(prev_snake_x);
+				segments_y.add(prev_snake_y);
+			}
+		}
+
+		// Move the snake segments.
+		for (int i = segments_x.size() - 1; i > 0; --i) {
+			segments_x[i] = segments_x[i - 1];
+			segments_y[i] = segments_y[i - 1];
+		}
+		if (segments_x.size()) {
+			segments_x[0] = prev_snake_x;
+			segments_y[0] = prev_snake_y;
+		}
+	}
+}
+
+v2 grid2world(int x, int y)
+{
+	float tile_dim = 5.0f;
+	int bw = 16 / 2;
+	int bh = 12 / 2;
+	float x_offset = tile_dim / 2.0f;
+	float y_offset = (float)(12 * tile_dim) - tile_dim / 2.0f;
+	return v2((float)(x-bw) * tile_dim + x_offset, -(float)(y+bh) * tile_dim + y_offset);
+}
+
+void draw_game(float dt)
+{
+	static sprite_t bg = sprite_make(app, "bg.ase");
+	bg.draw(b);
+
+	if (has_bomb) {
+		bomb.transform.p = grid2world(bomb_x, bomb_y);
+		bomb.update(dt);
+		bomb.draw(b);
+	}
+
+	snake_head.transform.p = grid2world(snake_x, snake_y);
+	snake_head.draw(b);
+
+	if (has_apple) {
+		apple.transform.p = grid2world(apple_x, apple_y);
+		apple.draw(b);
+	}
+
+	for (int i = 0; i < segments_x.size(); ++i) {
+		snake_segment.transform.p = grid2world(segments_x[i], segments_y[i]);
+		snake_segment.draw(b);
+	}
+
+	array<array<int>>& level = levels[current_level];
+	for (int y = 0; y < level.size(); ++y) {
+		for (int x = 0; x < level[y].size(); ++x) {
+			if (level[y][x] == 2) {
+				wall.transform.p = grid2world(x, y);
+				wall.draw(b);
+			} else if (level[y][x] == 3) {
+				weak_wall.transform.p = grid2world(x, y);
+				weak_wall.draw(b);
+			}
+		}
+	}
+
+	batch_flush(b);
+}
+
 int main(int argc, const char** argv)
 {
 	uint32_t app_options = CUTE_APP_OPTIONS_DEFAULT_GFX_CONTEXT | CUTE_APP_OPTIONS_WINDOW_POS_CENTERED;
@@ -240,23 +501,42 @@ int main(int argc, const char** argv)
 	mount_content_folder();
 	b = sprite_get_batch(app);
 
+	snake_head = sprite_make(app, "snake_head.ase");
+	snake_segment = sprite_make(app, "snake_segment.ase");
+	apple = sprite_make(app, "apple.ase");
+	wall = sprite_make(app, "wall.ase");
+	weak_wall = sprite_make(app, "weak_wall.ase");
+	bomb = sprite_make(app, "bomb.ase");
+	bomb.local_offset = v2(1, 1);
+
 	cute_preamble(app);
 	title_screen(app);
 
-	audio_t* eat = audio_load_wav("eat.wav");
 	audio_t* select = audio_load_wav("select.wav");
+
+	coroutine_t* gameplay_co = coroutine_make(do_gameplay);
+
+	array<key_button_t> wasd = { KEY_W, KEY_A, KEY_S, KEY_D };
+	array<key_button_t> arrows = { KEY_UP, KEY_LEFT, KEY_DOWN, KEY_RIGHT };
+	array<v2> dirs = { v2(0, -1), v2(-1, 0), v2(0, 1), v2(1, 0) };
 
 	while (app_is_running(app)) {
 		float dt = calc_dt();
 		app_update(app, dt);
 
-		if (key_was_pressed(app, KEY_SPACE)) {
-			sound_play(app, eat);
+		for (int i = 0; i < wasd.size(); ++i) {
+			if (key_was_pressed(app, wasd[i]) || key_was_pressed(app, arrows[i])) {
+				dir = dirs[i];
+			}
 		}
+
+		coroutine_resume(gameplay_co, dt);
+		draw_game(dt);
 
 		app_present(app);
 	}
 
+	coroutine_destroy(gameplay_co);
 	app_destroy(app);
 
 	return 0;
